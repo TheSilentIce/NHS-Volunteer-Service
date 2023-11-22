b2f4c600da37673496a588f142416647410fce00
YashChavan999 <yaash.chavan@gmail.com>
2023-11-22 15:46:21 -0700

    Added Radio Input Component

---
+++ nhs-website/src/components/forms/RadioInput.tsx
@@ -0,0 +1,28 @@
+interface RadioInputProps {
+	name: string;
+	id: string;
+	options: string[];
+}
+
+function RadioInput(properties: RadioInputProps) {
+	return (
+		<div className="pt-2">
+			{properties.name}
+			{properties.options.map((optionValue, index) => (
+				<div className="form-check" key={index}>
+					<input
+						className="form-check-input"
+						type="radio"
+						name="id"
+						id={properties.id + index}
+					/>
+					<label className="form-check-label" htmlFor={properties.id}>
+						{optionValue}
+					</label>
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default RadioInput;
