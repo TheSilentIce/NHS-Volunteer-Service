1ba44a273a07aea5bab9546f44bea23de11da50b
YashChavan999 <yaash.chavan@gmail.com>
2023-11-22 15:03:32 -0700

    Merged Activity Log into one file

--- nhs-website/src/pages/ActivityLog.tsx
+++ nhs-website/src/pages/ActivityLog.tsx
@@ -1,4 +1,5 @@
-import ActivityForm from "../components/forms/ActivityForm";
+import SelectMenu from "../components/forms/SelectMenu";
+import ShortAnswerInput from "../components/forms/ShortAnswerInput";
 
 export function ActivityLog() {
 	return (
@@ -6,7 +7,50 @@ export function ActivityLog() {
 			<div className="header-container container">
 				<h1>Activity Log</h1>
 			</div>
-			<ActivityForm />
+
+			<div className="container">
+				<form>
+					<div className="container-fluid">
+						<div className="row">
+							<div className="p-3" />
+
+							<div className="col">
+								<ShortAnswerInput
+									name="First Name"
+									id="firstName"
+									type="name"
+									placeholder=""
+								/>
+							</div>
+							<div className="col">
+								<ShortAnswerInput
+									name="Last Name"
+									id="lastName"
+									type="name"
+									placeholder=""
+								/>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col">
+								<div className="p-2" />
+
+								<ShortAnswerInput
+									name="Personal Email"
+									id="email"
+									type="email"
+									placeholder="example@mail.com"
+								/>
+								<SelectMenu
+									name="Grade"
+									id="grade"
+									options={[9, 10, 11, 12]}
+								></SelectMenu>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
 		</>
 	);
 }
